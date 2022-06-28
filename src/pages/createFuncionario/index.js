e6291d2fb98ea36e37c99cde8632f3c5b07fdeb1
rafael <rafaelfreire0@gmail.com>
2022-06-28 16:50:40 -0300

    up

--- src/pages/createFuncionario/index.js
+++ src/pages/createFuncionario/index.js
@@ -61,18 +61,18 @@ export default function Createfuncionario({ history }) {
             <h2 className='title'>Cadastrar Funcionario</h2>
 
             <form className='form' onSubmit={ handleSubmit }>
-                <input type='text' placeholder='Nome completo' value={nome} onChange={e => setNome(e.target.value)}/>
-                <input type='text' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
-                <input type='text' placeholder='Telefone' value={telefone} onChange={e => setTelefone(e.target.value)}/>
-                <input type='text' placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
-                <input type='password' placeholder='Senha' value={password} onChange={e => setPassword(e.target.value)}/>
-                <input type='text' placeholder='cpf' value={cpf} onChange={e => setCpf(e.target.value)}/>
-                <input type='text' placeholder='Rua' value={Rua} onChange={e => setRua(e.target.value)}/>
-                <input type='text' placeholder='Número' value={numero} onChange={e => setNumero(e.target.value)}/>
-                <input type='text' placeholder='Estado' value={estado} onChange={e => setEstado(e.target.value)}/>
-                <input type='text' placeholder='Cidade' value={cidade} onChange={e => setCidade(e.target.value)}/>
-                <input type='text' placeholder='CEP' value={cep} onChange={e => setCep(e.target.value)}/>
-                <input type='number' placeholder='ID Equipe' value={id} onChange={e => setId(e.target.value)}/>
+                <input required type='text' placeholder='Nome completo' value={nome} onChange={e => setNome(e.target.value)}/>
+                <input required type='text' placeholder='Email' value={email} onChange={e => setEmail(e.target.value)}/>
+                <input required type='text' placeholder='Telefone' value={telefone} onChange={e => setTelefone(e.target.value)}/>
+                <input required type='text' placeholder='username' value={username} onChange={e => setUsername(e.target.value)}/>
+                <input required type='password' placeholder='Senha' value={password} onChange={e => setPassword(e.target.value)}/>
+                <input required type='text' placeholder='cpf' value={cpf} onChange={e => setCpf(e.target.value)}/>
+                <input required type='text' placeholder='Rua' value={Rua} onChange={e => setRua(e.target.value)}/>
+                <input required type='text' placeholder='Número' value={numero} onChange={e => setNumero(e.target.value)}/>
+                <input required type='text' placeholder='Estado' value={estado} onChange={e => setEstado(e.target.value)}/>
+                <input required type='text' placeholder='Cidade' value={cidade} onChange={e => setCidade(e.target.value)}/>
+                <input required type='text' placeholder='CEP' value={cep} onChange={e => setCep(e.target.value)}/>
+                <input required type='number' placeholder='ID Equipe' value={id} onChange={e => setId(e.target.value)}/>
                 <button className='button' type='submit'><FiPlus /> Cadastrar</button>
             </form>
         </div>
